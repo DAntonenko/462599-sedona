ad7e40da5685e33ebd5a6fb01568cbc921d65a84
Danton <dm.antonenko1977@gmail.com>
2018-03-31 14:40:24 +0300

    js ф отдельный файл

---
+++ js/js-man-readable.js
@@ -0,0 +1,16 @@
+var button = document.querySelector(".hotel-search");
+var modal = document.querySelector(".modal");
+var form = modal.querySelector("form");
+
+modal.classList.add("js-modal-none");
+
+button.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modal.classList.toggle("js-modal-none");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		modal.classList.add("js-modal-none");
+};
