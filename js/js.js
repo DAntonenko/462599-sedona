28e83eaa64184a0e3219023f70481b4e6242d684
Danton <dm.antonenko1977@gmail.com>
2018-04-05 22:15:50 +0300

    добавлена анимация модального окна и фокус у чекбоксов

--- js/js.js
+++ js/js.js
@@ -1,10 +1,35 @@
-var button = document.querySelector(".hotel-search");
+var buttonSearch = document.querySelector(".hotel-search");
 var modal = document.querySelector(".modal");
+var form = modal.querySelector("form");
+var arrival = form.querySelector("[name=arrival-date]");
+var departure = form.querySelector("[name=departure-date]");
+var adults = form.querySelector("[name=adults-quantity]");
+var children = form.querySelector("[name=children-quantity]");
 
 modal.classList.add("js-modal-close");
 
-button.addEventListener("click", function (evt) {
+buttonSearch.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	modal.classList.toggle("js-modal-close");
 	modal.classList.toggle("js-modal-open");
+	modal.classList.remove("modal-error");
+	arrival.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!arrival.value || !departure.value || !adults.value || adults.value<1) {
+		evt.preventDefault();
+		modal.classList.add("modal-error");
+	}
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (modal.classList.contains("js-modal-open")) {
+			modal.classList.remove("js-modal-open");
+			modal.classList.add("js-modal-close");
+			modal.classList.remove("modal-error");
+		}
+	}
 });
